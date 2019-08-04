a7113587a2500779824a908878a214903fd584be
dollynasution <dolimartua.nasution@gmail.com>
2019-08-04 13:05:14 +0700

    commit function

--- pert3/function.js
+++ pert3/function.js
@@ -60,4 +60,16 @@ console.log(number)
 console.log(number * 5)
 console.log(Math.pow(number, 3))
 
+// 5000 = 5 detik, satuan dalam milli second, menentukan waktu delay
+setTimeout(function(){
+  console.log(`you've been waiting for 3 seconds`)
+},3000)
 
+const interval = setInterval(function(){
+  console.log('1 detik telah berlalu')
+}, 1000)
+
+function stop() {
+  clearInterval(interval)
+}
+setTimeout(stop, 3100)
